9f4c36e7d328c95f679cbc9ec994ecd15b5a53a4
Mimicoctopus1 <87786838+Mimicoctopus1@users.noreply.github.com>
2024-07-01 15:33:25 -0700

    YAY

---
+++ joke/init.lua
@@ -0,0 +1,15 @@
+minetest.register_chatcommand("joke", {
+    privs = {
+        interact = true
+    },
+
+    func = function(param) {
+        jokes = {
+            "joke",
+            "a really bad joke",
+            "This is a bad joke",
+            ""
+        }
+        return(jokes[math.random(0, jokes.length + 1)])
+    }
+})
