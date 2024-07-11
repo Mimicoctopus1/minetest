c864be853f5ddc2d6a287af2ab6c948917eb6e23
Mimicoctopus1 <87786838+Mimicoctopus1@users.noreply.github.com>
2024-07-11 15:26:50 -0700

    add dirt

--- dirt/init.lua
+++ dirt/init.lua
@@ -1,3 +1,20 @@
+minetest.register_tool("dirt:dirt_sword", {
+    description = "Indestructible, high-damage weapon.",
+    inventory_image = "textures/dirt_dirt_sword.png",
+    tool_capabilities = {
+        full_punch_interval = 0.5,
+        max_drop_level = 0,
+        groupcaps = {
+            crumbly = {
+                maxlevel = 2,
+                uses = 1,
+                times = 1
+            }
+        }
+
+    }
+})
+
 minetest.register_craft({
     output = "mcl_core:sand",
     type = "shapeless"
@@ -13,5 +30,14 @@ minetest.register_craft({
     recipe = {
         "mcl_core:sand"
         "mcl_farming:wheat"
+    }
+})
+
+minetest.register_craft({
+    output = "dirt:dirt_sword"
+    recipe = {
+        "mcl_core:dirt",
+        "mcl_core:dirt",
+        "mcl_core:stick"
     }
 })
